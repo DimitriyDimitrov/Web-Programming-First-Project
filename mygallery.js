3986bfe027f053b35a5f7047ce2214e98e224a80
DimitriyDimitrov <60811008+DimitriyDimitrov@users.noreply.github.com>
2020-02-09 09:43:13 +0200

    Add files via upload

--- mygallery.js
+++ mygallery.js
@@ -45,3 +45,21 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+
+// Scroll to Top button and Hover Effect //
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
